0448c72a6aa7b205afd94c0f4c78a6aaaa8fa842
Daniel <magicgames9@gmail.com>
2019-05-28 21:04:58 +0300

    improved code quality

--- src/app/lobby/lobby.component.ts
+++ src/app/lobby/lobby.component.ts
@@ -3,7 +3,8 @@ import { Router } from '@angular/router';
 import { LobbyService } from './lobby.service';
 import { ClientService } from '../client.service';
 import { LobbyStatusResultsResult } from '../common/interfaces/results';
-import { Observable } from 'rxjs';
+import { Subject, timer } from 'rxjs';
+import { debounce } from 'rxjs/operators';
 
 @Component({
   selector: 'app-lobby',
@@ -12,6 +13,8 @@ import { Observable } from 'rxjs';
 })
 
 export class LobbyComponent implements OnInit {
+  clientGameModeSelect$ = new Subject();
+
   lobbyId = this.clientService.getLobbyId();
   clientToken = this.clientService.getToken();
   clientName = this.clientService.getName();
@@ -22,9 +25,6 @@ export class LobbyComponent implements OnInit {
 
   lobbyStatus: LobbyStatusResultsResult;
   lobbyGameMode: string;
-  chosenGameMode: string = '';
-  currentGameMode: string = '';
-  isNewGameMode: boolean = false;
 
   constructor(
     private router: Router,
@@ -32,27 +32,30 @@ export class LobbyComponent implements OnInit {
     private clientService: ClientService) {}
 
   ngOnInit() {
-    this.init();
+    this.update();
+    setInterval(this.update, 1000);
+    this.clientGameModeSelect$
+      .pipe(debounce(() => timer(500))).subscribe((gameMode) => this.setGameMode(gameMode));
   }
 
-  private init() {
-    // delay is preventing from spamming from the UI
-    // ??: Should be really use a setInterval
-    setInterval(() => {
-      // if client isn't authorized
+  private update() {
       if (!this.clientToken) return;
-      // if statement for preventing sending the same request if the lobby is set
-      if (this.isNewGameMode) {
-        // only owners can change a gameMode
-        this.lobbyService.setLobbyGameMode(this.clientToken, this.lobbyId, this.chosenGameMode)
-        .subscribe(data => {
-          if(!data.error) {
-            this.isNewGameMode = false;
-          }
-        });
+      this.updateLobbyStatus();
+      this.updateLobbyClients();
+  }
+
+  private updateLobbyClients() {
+    this.lobbyService.getLobbyClients(this.lobbyId, this.clientToken).subscribe(data => {
+      const newLobbyClients = data.result;
+      // if there's a change in the lobbyClients
+      if (this.lobbyClients !== newLobbyClients) {
+        this.lobbyClients = newLobbyClients;
       }
-      // getting the lobby's status
-      this.lobbyService.getLobbyStatus(this.lobbyId, this.clientToken)
+    });
+  }
+
+  private updateLobbyStatus() {
+    this.lobbyService.getLobbyStatus(this.lobbyId, this.clientToken)
       .subscribe(data => {
         if (!data.error) {
           this.lobbyStatus = data.result;
@@ -63,27 +66,20 @@ export class LobbyComponent implements OnInit {
           // TOOD: print the error to the screen~
         }
       });
-      this.lobbyService.getLobbyClients(this.lobbyId, this.clientToken).subscribe(data => {
-        const newLobbyClients = data.result;
-        // if there's a change in the lobbyClients
-         if (this.lobbyClients !== newLobbyClients) {
-          this.lobbyClients = newLobbyClients;
+  }
+
+  private setGameMode(gameMode) {
+    // only owners can change a gameMode
+    this.lobbyService.setLobbyGameMode(this.clientToken, this.lobbyId, gameMode)
+      .subscribe(data => {
+        if(!data.error) {
+          // TODO: Implement game redirection?
         }
       });
-      // TODO: I believe this 'functionality' could have been written better, maybe change the client's URL according to his 'isOwner' value
-      // if a change in the lobby's status gameMode accured, change the current displaying element. (otherwise it would have just kept refreshing the element and it would have been ugly)
-      if (!this.isClientOwner && this.lobbyGameMode && this.lobbyGameMode !== this.currentGameMode) {
-        this.currentGameMode = this.lobbyGameMode;
-      }
-    }, 1000);
   }
 
   // this function is being called from the HTML. for Owners only
-  private setChosenGameMode(gameMode) {
-    if (this.chosenGameMode !== gameMode) {
-      this.chosenGameMode = gameMode;
-      this.isNewGameMode = true;
-    }
+  private onGameModeClick(gameMode) {
+    this.clientGameModeSelect$.next(gameMode);
   }
-
 }
